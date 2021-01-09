7c8ac7070cdedce221f118ae2a685c1cd84008f5
rankiliu <rankiliu@tencent.com>
2021-01-09 20:50:24 +0800

    first commit

---
+++ Demo/OOMDetectorDemo/RKLeakObject.h
@@ -0,0 +1,17 @@
+//
+//  RKLeakObject.h
+//  OOMDetector
+//
+//  Created by rankiliu on 2021/1/7.
+//  Copyright © 2021 shaodonggao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RKLeakObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo/OOMDetectorDemo/RKLeakObject.m
@@ -0,0 +1,13 @@
+//
+//  RKLeakObject.m
+//  OOMDetector
+//
+//  Created by rankiliu on 2021/1/7.
+//  Copyright © 2021 shaodonggao. All rights reserved.
+//
+
+#import "RKLeakObject.h"
+
+@implementation RKLeakObject
+
+@end
